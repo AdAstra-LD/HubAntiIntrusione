a645f930b087deb2e295ff1dfddfd5aa6c9c77fe
mtolkien <83503055+mtolkien@users.noreply.github.com>
2021-05-01 17:07:58 +0200

    Create testMQTT.py

---
+++ testMQTT.py
@@ -0,0 +1,47 @@
+# testMQTT
+# Created at 2021-05-01 13:43:08.132766
+
+import streams
+
+#import a module to access a net driver (wifi)
+from wireless import wifi
+
+#import a module for MQTT protocol
+from mqtt import mqtt
+
+#import the actual net driver for the specific board 
+from from espressif.esp32net import esp32wifi as wifi_driver
+
+streams.serial()
+
+#init the wifi_driver
+wifi_driver.auto_init()
+
+sleep(5000)
+print("Aspetto il collegamento...")
+try:
+    wifi.link("Vodafone-33577254", wifi.WIFI_WPA2, "FamigliaMacaro4")
+    print("Collegamento effettuato con successo!")
+except Exception:
+    print("Qualcosa è andato storto..")
+    while True:
+        sleep(1000)
+        
+
+try:
+    client = mqtt.Client("test", True)
+    for retry in range(5):
+        try:
+            client.connect("test.mosquitto.org", 30)
+            
+            
+            break
+        except Exception as e:
+            print("connecting to mosquitto...")
+    print("connected to mosquitto.")
+    
+    client.publish("stanza/sensore", 21, 2)
+    
+        
+    
+    
