e40490dc5f055b0ac0923654ea977a59be394fcb
AdAstra-LD <GIANNI.Laiso.gl@gmail.com>
2021-05-06 02:19:32 +0200

    Compatibilizzazione codice Keypad per Zerynth

--- keypad.py
+++ keypad.py
@@ -1,40 +1,50 @@
-from machine import Pin
-import time
+import timers
 
 lastChangeTime=0
 keyState=0
 State=0
 
 class KeyPad(object):
-    def __init__(self, row1: int=14, row2: int=27, row3: int=26, row4: int=25, col1: int=13, col2: int=21, col3: int=22, col4: int=23):
-        self._row1=Pin(row1,Pin.OUT)
-        self._row2=Pin(row2,Pin.OUT)
-        self._row3=Pin(row3,Pin.OUT)
-        self._row4=Pin(row4,Pin.OUT)
-        self._col1=Pin(col1,Pin.IN,Pin.PULL_DOWN)
-        self._col2=Pin(col2,Pin.IN,Pin.PULL_DOWN)
-        self._col3=Pin(col3,Pin.IN,Pin.PULL_DOWN)
-        self._col4=Pin(col4,Pin.IN,Pin.PULL_DOWN)
+    def __init__(self, row1: D14, row2: D27, row3: D26, row4: D25, col1: D13, col2: D21, col3: D22, col4: D23):
+        self._pinRow1 = row1
+        self._pinRow2 = row2
+        self._pinRow3 = row3
+        self._pinRow4 = row4
+        self._pinCol1 = col1
+        self._pinCol2 = col2
+        self._pinCol3 = col3
+        self._pinCol4 = col4
+        
+        pinMode(self._pinRow1, OUTPUT)
+        pinMode(self._pinRow2, OUTPUT)
+        pinMode(self._pinRow3, OUTPUT)
+        pinMode(self._pinRow4, OUTPUT)
+        pinMode(self._pinCol1, INPUT_PULLDOWN)
+        pinMode(self._pinCol2, INPUT_PULLDOWN)
+        pinMode(self._pinCol3, INPUT_PULLDOWN)
+        pinMode(self._pinCol4, INPUT_PULLDOWN)
         
     
     def scan(self):
         global lastChangeTime
-        nowTime=time.ticks_ms()
-        if(nowTime-lastChangeTime>10):
+        
+        nowTime = timers.now()
+        if(nowTime-lastChangeTime > 10):
             lastChangeTime=nowTime
-            if(self._readCol()!=0):
+            if(self._readCol() != 0):
                 State=self._readCol()
                 return State
             
     def _readRow1(self):
         self._setRow(1)
-        if(self._col1.value()==1):
+        
+        if(digitalRead(self._pinCol1)): #firstCol
             keyState='1'
-        elif(self._col2.value()==1):
+        elif(digitalRead(self._pinCol2)): #secondCol
             keyState='2'
-        elif(self._col3.value()==1):
+        elif(digitalRead(self._pinCol3)): #thirdCol
             keyState='3'
-        elif(self._col4.value()==1):
+        elif(digitalRead(self._pinCol4)): #fourthCol
             keyState='A'
         else:
             keyState=0
@@ -42,13 +52,14 @@ class KeyPad(object):
 
     def _readRow2(self):
         self._setRow(2)
-        if(self._col1.value()==1):
+        
+        if(digitalRead(self._pinCol1)):
             keyState='4'
-        elif(self._col2.value()==1):
+        elif(digitalRead(self._pinCol2):
             keyState='5'
-        elif(self._col3.value()==1):
+        elif(digitalRead(self._pinCol3):
             keyState='6'
-        elif(self._col4.value()==1):
+        elif(digitalRead(self._pinCol4):
             keyState='B'
         else:
             keyState=0 
@@ -56,13 +67,14 @@ class KeyPad(object):
 
     def _readRow3(self):
         self._setRow(3)
-        if(self._col1.value()==1):
+        
+        if(digitalRead(self._pinCol1)):
             keyState='7'
-        elif(self._col2.value()==1):
+        elif(digitalRead(self._pinCol2)):
             keyState='8'
-        elif(self._col3.value()==1):
+        elif(digitalRead(self._pinCol3)):
             keyState='9'
-        elif(self._col4.value()==1):
+        elif(digitalRead(self._pinCol4)):
             keyState='C'
         else:
             keyState=0 
@@ -70,13 +82,14 @@ class KeyPad(object):
     
     def _readRow4(self):
         self._setRow(4)
-        if(self._col1.value()==1):
+        
+        if(digitalRead(self._pinCol1)):
             keyState='*'
-        elif(self._col2.value()==1):
+        elif(digitalRead(self._pinCol2)):
             keyState='0'
-        elif(self._col3.value()==1):
+        elif(digitalRead(self._pinCol3)):
             keyState='#'
-        elif(self._col4.value()==1):
+        elif(digitalRead(self._pinCol4)):
             keyState='D'
         else:
             keyState=0
@@ -87,33 +100,33 @@ class KeyPad(object):
         data_buffer2=self._readRow2()
         data_buffer3=self._readRow3()
         data_buffer4=self._readRow4()
-        if (data_buffer1!=0):
+        if (data_buffer1 != 0):
             return data_buffer1
-        elif (data_buffer2!=0):
+        elif (data_buffer2 != 0):
             return data_buffer2
-        elif (data_buffer3!=0):
+        elif (data_buffer3 != 0):
             return data_buffer3
-        elif (data_buffer4!=0):
+        elif (data_buffer4 != 0):
             return data_buffer4
 
     def _setRow(self,num):
         if num==1:
-            self._row1.on()
-            self._row2.off()
-            self._row3.off()
-            self._row4.off()
+            self._pinRow1.on()
+            self._pinRow2.off()
+            self._pinRow3.off()
+            self._pinRow4.off()
         if num==2:
-            self._row1.off()
-            self._row2.on()
-            self._row3.off()
-            self._row4.off()
+            self._pinRow1.off()
+            self._pinRow2.on()
+            self._pinRow3.off()
+            self._pinRow4.off()
         if num==3:
-            self._row1.off()
-            self._row2.off()
-            self._row3.on()
-            self._row4.off()
+            self._pinRow1.off()
+            self._pinRow2.off()
+            self._pinRow3.on()
+            self._pinRow4.off()
         if num==4:
-            self._row1.off()
-            self._row2.off()
-            self._row3.off()
-            self._row4.on()
+            self._pinRow1.off()
+            self._pinRow2.off()
+            self._pinRow3.off()
+            self._pinRow4.on()
